0dce21b7bbb9b209db996f28851662238b889acd
Andreas Rizki Rizaldi <arizkirizaldi@mahasiswa.unikom.ac.id>
2021-07-02 01:20:44 +0700

    try to make users and give them roles

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 
 Route::get('/dashboard', function () {
-    return view('dashboard');
+    return view('layouts.dashboard');
 })->middleware(['auth'])->name('dashboard');
 
 Route::get('/test',function (){
