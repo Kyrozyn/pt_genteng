33179cc76a493dc3caf996ae696574bc09503ca7
Andreas Rizki Rizaldi <arizkirizaldi@mahasiswa.unikom.ac.id>
2021-08-18 14:23:32 +0700

    Add validation+B000 digit

--- app/Http/Livewire/Produk/Form.php
+++ app/Http/Livewire/Produk/Form.php
@@ -8,9 +8,15 @@ use Livewire\Component;
 class Form extends Component
 {
     public $status='Tambah';
-    public $id_pel,$nama, $deskripsi, $jenis="Aksesoris", $stok=0;
+    public $id_pel,$id_produk,$nama, $deskripsi, $jenis="Aksesoris", $stok=0;
     protected $listeners = ['edit' => 'editID','resetform'=>'res'];
-
+    protected $rules = [
+        'id_produk' => 'required|digits:3|unique:App\Models\produk,id|numeric',
+        'nama' => 'required',
+        'deskripsi' => 'required',
+        'jenis' => 'required',
+        'stok' => 'required',
+    ];
     public function render()
     {
         return view('livewire.produk.form');
@@ -24,7 +30,7 @@ class Form extends Component
     public function editID($id){
         $this->status = "Edit";
         $produk = produk::whereId($id)->first();
-        $this->id_pel = $produk->id;
+        $this->id_produk = str_pad($produk->id,3,'0',STR_PAD_LEFT);
         $this->nama = $produk->nama;
         $this->deskripsi = $produk->deskripsi;
         $this->jenis = $produk->jenis;
@@ -34,10 +40,13 @@ class Form extends Component
     public function submitAdd(){
         if($this->status == 'Tambah'){
             $produk = new produk();
+            $produk->id = $this->id_produk;
         }
         elseif($this->status == 'Edit'){
+            $this->rules['id_pegawai_training'] = 'required|digits:3|numeric';
             $produk = produk::whereId($this->id_pel)->first();
         }
+        $this->validate();
         $produk->nama = $this->nama;
         $produk->deskripsi = $this->deskripsi;
         $produk->jenis = $this->jenis;
--- resources/views/livewire/produk/form.blade.php
+++ resources/views/livewire/produk/form.blade.php
@@ -12,12 +12,34 @@
                 </div>
                 <div class="block-content">
                     <div class="form-group">
+                        @foreach ($errors->all() as $error)
+                            <div class="alert alert-danger" role="alert">
+                                <p class="mb-0">{{$error}}</p>
+                            </div>
+                        @endforeach
+                    </div>
+                    <div class="form-group">
+                        <label for="nama">ID Produk</label>
+                        <div class="input-group">
+
+                            <div class="input-group-prepend">
+                                                    <span class="input-group-text">
+                                                        B
+                                                    </span>
+                            </div>
+                            <input type="number" class="form-control" id="nama" name="nama" placeholder="ID Produk" wire:model="id_produk">
+                            @error('id_produk')  @enderror
+                        </div>
+                    </div>
+                    <div class="form-group">
                         <label for="nama">Nama Produk</label>
                         <input type="text" class="form-control" id="nama" name="nama" placeholder="Nama Produk" wire:model="nama">
+                        @error('nama') <span class="text-danger">{{ $message }}</span> @enderror
                     </div>
                     <div class="form-group">
                         <label for="Deskripsi">Deskripsi Produk</label>
                         <textarea type="text" class="form-control" id="Deskripsi" name="Deskripsi" placeholder="Deskripsi Produk" wire:model="deskripsi"></textarea>
+                        @error('deskripsi') <span class="text-danger">{{ $message }}</span> @enderror
                     </div>
                     <div class="form-group">
                         <label for="jenis">Jenis Produk</label>
@@ -25,14 +47,17 @@
                             <option value="Aksesoris">Aksesoris</option>
 J                            <option value="Genteng">Genteng</option>
                         </select>
+                        @error('jenis') <span class="text-danger">{{ $message }}</span> @enderror
                     </div>
                     <div class="form-group">
                         <label for="Stok">Stok</label>
                         <input type="number" class="form-control" id="Stok" name="Stok" wire:model="stok">
+                        @error('stok') <span class="text-danger">{{ $message }}</span> @enderror
                     </div>
                 </div>
                 <div class="block-content block-content-full text-right bg-light" @if(($nama == null OR $deskripsi == null OR $jenis == null)) style="display: none" @endif>
-                    <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal" wire:click="submitAdd">{{$status}} Produk</button>
+                    <button type="button" class="btn btn-sm btn-primary" wire:click="submitAdd">{{$status}} Produk</button>
+{{--                    <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal" wire:click="submitAdd">{{$status}} Produk</button>--}}
                 </div>
             </div>
         </div>
